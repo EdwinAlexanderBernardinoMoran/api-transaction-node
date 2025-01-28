08e476248f20ada42f6d2e1a330238aa73a135cb
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-01-28 09:13:22 -0600

    Simplify webhook response to return raw data instead of structured event and data

--- controller/transaction.controller.js
+++ controller/transaction.controller.js
@@ -39,6 +39,6 @@ export const getTransactions = async (req, res) => {
 }
 
 export const getTransactionWebhook = async (req, res) => {
-    const { event, data } = req.body;
-    res.status(200).json({ message: 'Webhook received', event, data });
+    const data = req.body;
+    res.status(200).json({ message: data});
 };
